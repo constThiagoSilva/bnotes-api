bad6dc89252566f64f91f5935494d3a29b6f3981
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-28 11:04:31 -0300

    feat: make a structure to handle any errors

--- src/domain/usecases/saveNotesUseCase/saveNotesUseCase.spec.ts
+++ src/domain/usecases/saveNotesUseCase/saveNotesUseCase.spec.ts
@@ -1,8 +1,21 @@
 class SaveNotesUseCase implements ISaveNotesUseCase{
     constructor(private saveNotesRepository: SaveNotesRepository) {}
 
-    async save(newNote: NewNote) { 
-        await this.saveNotesRepository.save(newNote)
+    async save(newNote: NewNote): Promise<{ok?: string, error?: IError}> {
+            const isThrowSaveNotesError = ThrowSaveNotesError(newNote)
+
+            if (isThrowSaveNotesError) {
+                return {
+                    error: isThrowSaveNotesError
+                }
+            }
+            
+            await this.saveNotesRepository.save(newNote)
+
+            return {
+                ok: 'ok'
+            }
+
     }
 }
 class SaveNotesRepositorySpy implements SaveNotesRepository {
@@ -22,9 +35,25 @@ class SaveNotesRepositorySpy implements SaveNotesRepository {
     }
 }
 
+const ThrowSaveNotesError = (newNote: NewNote): IError | null => {
+    if (!newNote.author) {
+        return {
+            code: 500,
+            message: new ProvidedParamsError('author')
+        }
+    }
+
+    return null
+}
+
+class ProvidedParamsError extends Error {
+    constructor(incorrectlyParameterProvided: string) {
+        super(`parameter: ${incorrectlyParameterProvided}, not provided`)
+    }
+}
 
 interface ISaveNotesUseCase {
-    save(newNote: NewNote): Promise<void>
+    save(newNote: NewNote): Promise<{ok?: string, error?: IError}>
 }
 
 interface SaveNotesRepository {
@@ -42,6 +71,15 @@ interface NewNote {
     author: string,
     title: string,
     content: string
+}
+
+interface IError {
+    code: number,
+    message: ProvidedParamsError
+}
+interface SaveNotesError {
+    code: number;
+    message: string
 }
 
 const makeSut = () => {
@@ -63,5 +101,21 @@ describe('Save Notes Use Case', () => {
         await sut.save(aNewNote)
 
         expect(saveNotesRepository.getNote).not.toBe(null)
+    })
+
+
+    it('should return 500 if author require parameter is not provided', async () => {
+        const {sut} = makeSut()
+        const aNewNote: NewNote = {
+            author: '',
+            title: 'any_title',
+            content: 'any_content',
+        }
+
+        const {error} = await sut.save(aNewNote)
+
+        expect(error?.code).toBe(500)
+        expect(error?.message.message).toBe('parameter: author, not provided')
+
     })
 })
