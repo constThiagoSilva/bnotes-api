4d67f8eb135ff368b9343b7d5be1a00c3cca834e
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-28 21:53:39 -0300

    feat: finish save-notes-repository-spy test (i think)

--- src/domain/usecases/saveNotesUseCase/mocks/repository/SaveNotesRepositorySpy.spec.ts
+++ src/domain/usecases/saveNotesUseCase/mocks/repository/SaveNotesRepositorySpy.spec.ts
@@ -1,6 +1,6 @@
+import { Note } from "../../../../models/Note"
 import { NewNote } from "../../interfaces/iNewNote"
 import { SaveNotesRepositorySpy } from "./SaveNotesRepositorySpy"
-
 
 describe('Save Notes Repository Spy',() => {
     it('should return 500 if author parameter is not provided correctly', async () => {
@@ -41,5 +41,43 @@ describe('Save Notes Repository Spy',() => {
 
         expect(error?.code).toBe(500)
         expect(error?.message.message).toBe('Parameter content not provided')
+    })
+    it('should getNote return same note passed in parameters', async () => {
+        const sut = new SaveNotesRepositorySpy()
+        const MOCK_NOTE: NewNote = {
+            author: 'any_author',
+            title: 'any_title',
+            content: 'any_content'
+        }
+
+        const MOCK_NEW_NOTE: Note = {
+            author: 'any_author',
+            title: 'any_title',
+            content: 'any_content',
+            createAt: new Date()
+        }
+
+        await sut.save(MOCK_NOTE)
+
+        expect(sut.getNote()).toEqual(MOCK_NEW_NOTE)
+    })
+    it('should return a note with passed in parameters', async () => {
+        const sut = new SaveNotesRepositorySpy()
+        const MOCK_NOTE: NewNote = {
+            author: 'any_author',
+            title: 'any_title',
+            content: 'any_content'
+        }
+
+        const MOCK_NEW_NOTE: Note = {
+            author: 'any_author',
+            title: 'any_title',
+            content: 'any_content',
+            createAt: new Date()
+        }
+
+        const {note} = await sut.save(MOCK_NOTE)
+
+        expect(note).toEqual(MOCK_NEW_NOTE)
     })
 })
