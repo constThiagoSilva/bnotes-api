431266d552b910fc34cc55daa3f854b678bd6454
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-31 21:11:57 -0300

    feat: add "should change the status of note Active to Trash" test

--- src/domain/usecases/deleteNoteUseCase/deleteNoteUseCase.spec.ts
+++ src/domain/usecases/deleteNoteUseCase/deleteNoteUseCase.spec.ts
@@ -1,31 +1,40 @@
 import { IError } from "../../helpers/errors/saveNotesUseCaseError/interfaces/IError";
 import { ProvidedParamsError } from "../../helpers/errors/saveNotesUseCaseError/ProviedParamsError";
+import { Note } from "../../models/Note";
 
 interface IDeleteNoteUseCase {
-  delete(id: string): Promise<{ error: IError | null; isDeleted: boolean }>;
+  delete(id: string): Promise<{ error: IError | null; deleteNote: Note | null }>;
 }
 
 interface DeleteNoteRepository {
-    delete(id: string): Promise<boolean>
+    delete(id: string): Promise<Note | null>
 }
 
 class DeleteNoteRepositorySpy implements DeleteNoteRepository{
-    delete(id: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    async delete(id: string): Promise<Note | null> {
+        return {
+            author: 'any_author',
+            content: 'any_content',
+            id: '1',
+            title: 'any_title',
+            status: 'Trash',
+            updateAt: new Date('2022-10-31'),
+            createAt: new Date('2022-10-31'),
+        }
     }
 }
 
 class DeleteNoteUseCase implements IDeleteNoteUseCase {
     constructor(private deleteNoteRepository: DeleteNoteRepository) {}
 
-  async delete(id: string): Promise<{ error: IError | null; isDeleted: boolean }> {
+  async delete(id: string): Promise<{ error: IError | null; deleteNote: Note | null }> {
     if (!id) {
         return {
             error: {
                 code: 500,
                 message: new ProvidedParamsError("id"),
             },
-            isDeleted: false,
+            deleteNote: null,
         };
     }
 
@@ -33,24 +42,31 @@ class DeleteNoteUseCase implements IDeleteNoteUseCase {
 
 
     return {
-        isDeleted: true,
+        deleteNote: isDeleted,
         error: null
     }
   }
 }
 
+const makeSut = () => {
+    const deleteNoteRepository = new DeleteNoteRepositorySpy()
+    const sut = new DeleteNoteUseCase(deleteNoteRepository);
+
+    return {sut, deleteNoteRepository}
+}
+
 describe("Delete Note Use Case", () => {
     it("should change the status of note 'Active' to 'Trash'", async () => {
-        const sut = new DeleteNoteUseCase();
+        const {sut} = makeSut()
         const MOCK_NOTE_ID = '1'
 
-        const {isDeleted} = await sut.delete(MOCK_NOTE_ID)
+        const {deleteNote} = await sut.delete(MOCK_NOTE_ID)
 
-        expect(isDeleted).toBeTruthy()
+        expect(deleteNote?.status).toBe('Trash')
     })
 
   it("should return 500 if no id of note is provided", async () => {
-    const sut = new DeleteNoteUseCase();
+    const {sut} = makeSut()
 
     const { error } = await sut.delete("");
 
