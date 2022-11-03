c0b5c9a01525d4c5d975ce986cd56bf3f6572ab1
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-02 23:46:59 -0300

    feat: add "should return 500 if noteId not provided" test

--- src/domain/repositories/changeStatusNotesToTrashRepository/changeStatusNotesToTrashRepository.spec.ts
+++ src/domain/repositories/changeStatusNotesToTrashRepository/changeStatusNotesToTrashRepository.spec.ts
@@ -1,7 +1,9 @@
+import { IError } from "../../helpers/errors/saveNotesUseCaseError/interfaces/IError";
+import { ProvidedParamsError } from "../../helpers/errors/saveNotesUseCaseError/ProviedParamsError";
 import { Note } from "../../models/Note"
 
 interface IChangeStatusNotesToTrash {
-    changeStatusToTrash(noteId: string): Promise<Note | null>
+    changeStatusToTrash(noteId: string): Promise<{trashedNote: Note | null, error: IError | null}>
 }
 interface Database {
     changeStatusToTrash(noteId: string): Promise<Note | null>
@@ -44,15 +46,28 @@ class DatabaseSpy implements Database{
 }
 
 class ChangeStatusNotesToTrash implements IChangeStatusNotesToTrash{
-    changeStatusToTrash(noteId: string): Promise<Note | null> {
-        const trashedNote = new DatabaseSpy().changeStatusToTrash(noteId)
+    async changeStatusToTrash(noteId: string): Promise<{trashedNote: Note | null, error: IError | null}> {
+        if (!noteId) {
+            return {
+                error: {
+                    code: 500,
+                    message: new ProvidedParamsError('noteId')
+                },
+                trashedNote: null
+            }
+        }
 
-        return trashedNote
+        const trashedNote = await new DatabaseSpy().changeStatusToTrash(noteId)
+
+        return {
+            trashedNote,
+            error: null,
+        }
     }
 }
 
-describe('Delete Notes Repository', () => {
-    it('should delete a note with id', async () => {
+describe('Change Status Notes To Trash Repository', () => {
+    it('should change status note to trash with id', async () => {
         const sut = new ChangeStatusNotesToTrash()
         const MOCK_NOTE: Note = {
             id: '1',
@@ -64,8 +79,16 @@ describe('Delete Notes Repository', () => {
             updateAt: new Date('2020-01-10'),
         }
 
-        const trashedNote = await sut.changeStatusToTrash(MOCK_NOTE.id)
+        const {trashedNote} = await sut.changeStatusToTrash(MOCK_NOTE.id)
 
         expect(trashedNote?.status).toBe('Trash')
+    })
+    it('should return 500 if noteId not provided', async () => {
+        const sut = new ChangeStatusNotesToTrash()
+
+        const {error} = await sut.changeStatusToTrash('')
+
+        expect(error?.code).toBe(500)
+        expect(error?.message.message).toBe('parameter: noteId, not provided')
     })
 })
