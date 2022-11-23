a3d0dbdac64bb35c1407e61330624214c5b0e40c
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-23 20:35:50 -0300

    feat: add test to ProvidedParamsError should show the same message

--- src/domain/helpers/errors/saveNotesUseCaseError/ProviedParamsError.spec.ts
+++ src/domain/helpers/errors/saveNotesUseCaseError/ProviedParamsError.spec.ts
@@ -1,9 +1,14 @@
 import { ProvidedParamsError } from "./ProviedParamsError"
 
 describe('ProvidedParamsError Class Error', () => {
-    it.only('should throw an error', () => {
+    it('should throw an error', () => {
         const sut = () => {throw new ProvidedParamsError('any')}
 
         expect(sut).toThrowError(new ProvidedParamsError('any'))
+    })
+    it('should return a message: parameter: any, not provided', () => {
+        const sut = new ProvidedParamsError('any')
+
+        expect(sut.message).toBe("parameter: any, not provided")
     })
 })
