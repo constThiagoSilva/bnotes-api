bd8409456d81e2493236ef04038a273eba56d926
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-23 20:52:58 -0300

    feat: add should return a null if note with id provided is not exist test

--- src/domain/repositories/mocks/repository/DatabaseSpy.spec.ts
+++ src/domain/repositories/mocks/repository/DatabaseSpy.spec.ts
@@ -9,4 +9,12 @@ describe("Database Spy", () => {
 
     expect(noteChangedStatus?.status).toBe('Trash')
   });
+  it('should return a null if note with id provided is not exist, in method "changeStatusToTrash"', async () => {
+    const sut = new DatabaseSpy();
+    const mockIdNote = 'not id existing'
+
+    const noteChangedStatus = await  sut.changeStatusToTrash(mockIdNote)
+
+    expect(noteChangedStatus?.status).toBeFalsy()
+  });
 });
