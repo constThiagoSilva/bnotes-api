a36dcfde659a1eb2b754eb96527cd313a5cf007d
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-23 20:42:42 -0300

    feat: add factory to throw-sabe-notes-error test

--- src/domain/helpers/errors/saveNotesUseCaseError/ThrowSaveNotesError.spec.ts
+++ src/domain/helpers/errors/saveNotesUseCaseError/ThrowSaveNotesError.spec.ts
@@ -1,9 +1,15 @@
-import { ProvidedParamsError } from "./ProviedParamsError"
+import { NewNote } from "../../../usecases/saveNotesUseCase/interfaces/iNewNote"
 import { ThrowSaveNotesError } from "./ThrowSaveNotesError"
+
+const makeSut = (fakeNote: NewNote) => {
+    const sut = ThrowSaveNotesError(fakeNote)
+
+    return {sut}
+}
 
 describe('Throw Save Notes Error', () => {
     it('should return an error 500 with message: parameter: author, not provided, if author is not provided', () => {
-        const sut = ThrowSaveNotesError({author: '', content: 'any_content', title: 'any_title'})
+        const {sut} = makeSut({author: '', content: 'any_content', title: 'any_title'})
 
         expect(sut?.code).toBe(500)
         expect(sut?.message.message).toBe("parameter: author, not provided")
