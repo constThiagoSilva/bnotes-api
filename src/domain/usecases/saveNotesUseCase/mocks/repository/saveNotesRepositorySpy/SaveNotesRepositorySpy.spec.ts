074031d4e9bb9682a37520dd1bee9a632361400a
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-24 18:53:19 -0300

    refactor: refactor save-notes-repository-spy

--- src/domain/usecases/saveNotesUseCase/mocks/repository/saveNotesRepositorySpy/SaveNotesRepositorySpy.spec.ts
+++ src/domain/usecases/saveNotesUseCase/mocks/repository/saveNotesRepositorySpy/SaveNotesRepositorySpy.spec.ts
@@ -1,98 +1,94 @@
-import { Note } from "../../../../../models/Note"
-import { NewNote } from "../../../interfaces/iNewNote"
-import { SaveNotesRepositorySpy } from "./SaveNotesRepositorySpy"
+import { Note } from "../../../../../models/Note";
+import { NewNote } from "../../../interfaces/iNewNote";
+import { SaveNotesRepositorySpy } from "./SaveNotesRepositorySpy";
 
-describe('Save Notes Repository Spy',() => {
-    it('should return 500 if author parameter is not provided correctly', async () => {
-        const sut = new SaveNotesRepositorySpy()
-        const MOCK_NOTE: NewNote = {
-            author: '',
-            title: 'any_title',
-            content: 'any_content'
-        }
+describe("Save Notes Repository Spy", () => {
+  it("should getNote return same note passed in parameters", async () => {
+    const sut = new SaveNotesRepositorySpy();
+    const mockFakeNewNote: NewNote = {
+      author: "any_author",
+      title: "any_title",
+      content: "any_content",
+    };
+    const mockFakeStorageNote: Note = {
+      id: "1",
+      author: "any_author",
+      title: "any_title",
+      content: "any_content",
+      updateAt: null,
+      createAt: new Date(),
+      status: "Active",
+    };
 
-        const {error} = await sut.save(MOCK_NOTE)
+    const { note } = await sut.save(mockFakeNewNote);
+    const noteId = String(note?.id);
 
-        expect(error?.code).toBe(500)
-        expect(error?.message.message).toBe('Parameter author not provided')
-    })
-    it('should return 500 if title parameter is not provided correctly', async () => {
-        const sut = new SaveNotesRepositorySpy()
-        const MOCK_NOTE: NewNote = {
-            author: 'any_author',
-            title: '',
-            content: 'any_content'
-        }
+    expect(sut.getNoteById(noteId)?.id).toBe(mockFakeStorageNote.id);
+    expect(sut.getNoteById(noteId)?.author).toBe(mockFakeStorageNote.author);
+    expect(sut.getNoteById(noteId)?.title).toBe(mockFakeStorageNote.title);
+    expect(sut.getNoteById(noteId)?.content).toBe(mockFakeStorageNote.content);
+  });
+  it("should return a note with passed in parameters", async () => {
+    const sut = new SaveNotesRepositorySpy();
+    const mockFakeNewNote: NewNote = {
+      author: "any_author",
+      title: "any_title",
+      content: "any_content",
+    };
+    const mockFakeStorageNote: Note = {
+      id: "1",
+      author: "any_author",
+      title: "any_title",
+      content: "any_content",
+      updateAt: null,
+      createAt: new Date(),
+      status: "Active",
+    };
 
-        const {error} = await sut.save(MOCK_NOTE)
+    const { note } = await sut.save(mockFakeNewNote);
 
-        expect(error?.code).toBe(500)
-        expect(error?.message.message).toBe('Parameter title not provided')
-    })
-    it('should return 500 if content parameter is not provided correctly', async () => {
-        const sut = new SaveNotesRepositorySpy()
-        const MOCK_NOTE: NewNote = {
-            author: 'any_author',
-            title: 'any_title',
-            content: ''
-        }
+    expect(note?.id).toBe(mockFakeStorageNote.id);
+    expect(note?.author).toBe(mockFakeStorageNote.author);
+    expect(note?.title).toBe(mockFakeStorageNote.title);
+    expect(note?.content).toBe(mockFakeStorageNote.content);
+  });
+  it("should return 500 if author parameter is not provided correctly", async () => {
+    const sut = new SaveNotesRepositorySpy();
+    const mockFakeNote: NewNote = {
+      author: "",
+      title: "any_title",
+      content: "any_content",
+    };
 
-        const {error} = await sut.save(MOCK_NOTE)
+    const { error } = await sut.save(mockFakeNote);
 
-        expect(error?.code).toBe(500)
-        expect(error?.message.message).toBe('Parameter content not provided')
-    })
-    it('should getNote return same note passed in parameters', async () => {
-        const sut = new SaveNotesRepositorySpy()
-        const MOCK_NOTE: NewNote = {
-            author: 'any_author',
-            title: 'any_title',
-            content: 'any_content'
-        }
+    expect(error?.code).toBe(500);
+    expect(error?.message.message).toBe("Parameter author not provided");
+  });
+  it("should return 500 if title parameter is not provided correctly", async () => {
+    const sut = new SaveNotesRepositorySpy();
+    const mockFakeNote: NewNote = {
+      author: "any_author",
+      title: "",
+      content: "any_content",
+    };
 
-        const MOCK_NEW_NOTE: Note = {
-            id: '1',
-            author: 'any_author',
-            title: 'any_title',
-            content: 'any_content',
-            updateAt: null,
-            createAt: new Date(),
-            status: "Active"
-        }
+    const { error } = await sut.save(mockFakeNote);
 
-        const {note} = await sut.save(MOCK_NOTE)
-        const noteId = String(note?.id)
+    expect(error?.code).toBe(500);
+    expect(error?.message.message).toBe("Parameter title not provided");
+  });
+  it("should return 500 if content parameter is not provided correctly", async () => {
+    const sut = new SaveNotesRepositorySpy();
+    const mockFakeNote: NewNote = {
+      author: "any_author",
+      title: "any_title",
+      content: "",
+    };
 
-        
-        expect(sut.getNoteById(noteId)?.id).toBe(MOCK_NEW_NOTE.id)
-        expect(sut.getNoteById(noteId)?.author).toBe(MOCK_NEW_NOTE.author)
-        expect(sut.getNoteById(noteId)?.title).toBe(MOCK_NEW_NOTE.title)
-        expect(sut.getNoteById(noteId)?.content).toBe(MOCK_NEW_NOTE.content)
-    })
-    it('should return a note with passed in parameters', async () => {
-        const sut = new SaveNotesRepositorySpy()
-        const MOCK_NOTE: NewNote = {
-            author: 'any_author',
-            title: 'any_title',
-            content: 'any_content'
-        }
+    const { error } = await sut.save(mockFakeNote);
 
-        const MOCK_NEW_NOTE: Note = {
-            id: '1',
-            author: 'any_author',
-            title: 'any_title',
-            content: 'any_content',
-            updateAt: null,
-            createAt: new Date(),
-            status: 'Active'
-        }
-        
-        const {note} = await sut.save(MOCK_NOTE)
-        
-
-        expect(note?.id).toBe(MOCK_NEW_NOTE.id)
-        expect(note?.author).toBe(MOCK_NEW_NOTE.author)
-        expect(note?.title).toBe(MOCK_NEW_NOTE.title)
-        expect(note?.content).toBe(MOCK_NEW_NOTE.content)
-    })
-})
+    expect(error?.code).toBe(500);
+    expect(error?.message.message).toBe("Parameter content not provided");
+  });
+});
