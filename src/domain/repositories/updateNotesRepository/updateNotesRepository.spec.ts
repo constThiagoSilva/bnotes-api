0807903e7dac6896714920c9bdde6a54f7894e1d
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-25 20:04:40 -0300

    refactor: refac update-notes-repo

--- src/domain/repositories/updateNotesRepository/updateNotesRepository.spec.ts
+++ src/domain/repositories/updateNotesRepository/updateNotesRepository.spec.ts
@@ -1,32 +1,40 @@
 import { Note } from "../../models/Note"
 import { NewNote } from "../../usecases/saveNotesUseCase/interfaces/iNewNote"
+import { DatabaseSpy } from "../mocks/repository/DatabaseSpy"
+import { Database } from "../mocks/repository/interfaces/Database"
 
-class UpdateNotesRepository {
+interface IUpdateNotesRepository {
+    updateNote(noteId: string, newNote: NewNote): Promise<Note | null>
+}
+
+class UpdateNotesRepository implements IUpdateNotesRepository{
+    constructor (private databaseSpy: Database) {}
+
     async updateNote(noteId: string, newNote: NewNote): Promise<Note | null> {
-        return {
-            id: '1',
-            author: 'other_author',
-            content: 'other_content',
-            title: 'other_title',
-            status: 'Active',
-            createAt: new Date("2022-10-31"),
-            updateAt: new Date("2022-10-31"),
-        }
+        const updatedNote = await this.databaseSpy.update(noteId, newNote)
+
+        return updatedNote
     }
 }
 
 describe('Update Notes Repository', () => {
     it('should update a exist note', async () => {
-        const sut = new UpdateNotesRepository()
+        const databaseSpy = new DatabaseSpy()
+        const sut = new UpdateNotesRepository(databaseSpy)
         const mockNoteId = '1'
+        const mockNewNote: NewNote = {
+            author: 'any_author',
+            content: 'any_content',
+            title: 'any_title'
+        }
         const mockUpdateNote: NewNote = {
-            author: 'other_author',
+            author: 'any_author',
             content: 'other_content',
             title: 'other_title'
         }
         const mockUpdatedNote: Note = {
             id: '1',
-            author: 'other_author',
+            author: 'any_author',
             content: 'other_content',
             title: 'other_title',
             status: 'Active',
@@ -34,6 +42,7 @@ describe('Update Notes Repository', () => {
             updateAt: new Date("2022-10-31"),
         }
 
+        await databaseSpy.create(mockNewNote)
         const updatedNote = await sut.updateNote(mockNoteId, mockUpdateNote)
 
         expect(updatedNote).toEqual(mockUpdatedNote)
