7d893b86e52f0ff7650ee2deee5273743a7fb719
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-25 20:10:26 -0300

    feat: add error test to update-notes-repo

--- src/domain/repositories/updateNotesRepository/updateNotesRepository.spec.ts
+++ src/domain/repositories/updateNotesRepository/updateNotesRepository.spec.ts
@@ -1,50 +1,84 @@
-import { Note } from "../../models/Note"
-import { NewNote } from "../../usecases/saveNotesUseCase/interfaces/iNewNote"
-import { DatabaseSpy } from "../mocks/repository/DatabaseSpy"
-import { Database } from "../mocks/repository/interfaces/Database"
+import { IError } from "../../helpers/errors/saveNotesUseCaseError/interfaces/IError";
+import { ProvidedParamsError } from "../../helpers/errors/saveNotesUseCaseError/ProviedParamsError";
+import { Note } from "../../models/Note";
+import { NewNote } from "../../usecases/saveNotesUseCase/interfaces/iNewNote";
+import { DatabaseSpy } from "../mocks/repository/DatabaseSpy";
+import { Database } from "../mocks/repository/interfaces/Database";
 
 interface IUpdateNotesRepository {
-    updateNote(noteId: string, newNote: NewNote): Promise<Note | null>
+  updateNote(noteId: string, newNote: NewNote): Promise<{updatedNote: Note | null, error: IError | null}>;
 }
 
-class UpdateNotesRepository implements IUpdateNotesRepository{
-    constructor (private databaseSpy: Database) {}
+class UpdateNotesRepository implements IUpdateNotesRepository {
+  constructor(private databaseSpy: Database) {}
 
-    async updateNote(noteId: string, newNote: NewNote): Promise<Note | null> {
-        const updatedNote = await this.databaseSpy.update(noteId, newNote)
+  async updateNote(noteId: string, newNote: NewNote): Promise<{updatedNote: Note | null, error: IError | null}> {
+    const updatedNote = await this.databaseSpy.update(noteId, newNote);
 
-        return updatedNote
-    }
+    return {
+        updatedNote,
+        error: {
+            message: new ProvidedParamsError('author'),
+            code: 500
+        }
+    };
+  }
 }
 
-describe('Update Notes Repository', () => {
-    it('should update a exist note', async () => {
-        const databaseSpy = new DatabaseSpy()
-        const sut = new UpdateNotesRepository(databaseSpy)
-        const mockNoteId = '1'
-        const mockNewNote: NewNote = {
-            author: 'any_author',
-            content: 'any_content',
-            title: 'any_title'
-        }
-        const mockUpdateNote: NewNote = {
-            author: 'any_author',
-            content: 'other_content',
-            title: 'other_title'
-        }
-        const mockUpdatedNote: Note = {
-            id: '1',
-            author: 'any_author',
-            content: 'other_content',
-            title: 'other_title',
-            status: 'Active',
-            createAt: new Date("2022-10-31"),
-            updateAt: new Date("2022-10-31"),
-        }
+const makeSut = () => {
+  const databaseSpy = new DatabaseSpy();
+  const sut = new UpdateNotesRepository(databaseSpy);
 
-        await databaseSpy.create(mockNewNote)
-        const updatedNote = await sut.updateNote(mockNoteId, mockUpdateNote)
+  return { sut, databaseSpy };
+};
 
-        expect(updatedNote).toEqual(mockUpdatedNote)
-    })
-})
+describe("Update Notes Repository", () => {
+  it("should update a exist note", async () => {
+    const { sut, databaseSpy } = makeSut();
+    const mockNoteId = "1";
+    const mockNewNote: NewNote = {
+      author: "any_author",
+      content: "any_content",
+      title: "any_title",
+    };
+    const mockUpdateNote: NewNote = {
+      author: "any_author",
+      content: "other_content",
+      title: "other_title",
+    };
+    const mockUpdatedNote: Note = {
+      id: "1",
+      author: "any_author",
+      content: "other_content",
+      title: "other_title",
+      status: "Active",
+      createAt: new Date("2022-10-31"),
+      updateAt: new Date("2022-10-31"),
+    };
+
+    await databaseSpy.create(mockNewNote);
+    const {updatedNote} = await sut.updateNote(mockNoteId, mockUpdateNote);
+
+    expect(updatedNote).toEqual(mockUpdatedNote);
+  });
+  it("should return a 500 if author param is not provided correctly", async () => {
+    const { sut, databaseSpy } = makeSut();
+    const mockNoteId = "1";
+    const mockNewNote: NewNote = {
+      author: "",
+      content: "any_content",
+      title: "any_title",
+    };
+    const mockUpdateNote: NewNote = {
+      author: "any_author",
+      content: "other_content",
+      title: "other_title",
+    };
+
+    await databaseSpy.create(mockNewNote);
+    const { error } = await sut.updateNote(mockNoteId, mockUpdateNote);
+
+    expect(error?.message.message).toBe("parameter: author, not provided");
+    expect(error?.code).toBe(500);
+  });
+});
