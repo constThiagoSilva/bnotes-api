c400d0e5720b4039520c0194779203428b2b4b65
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-25 20:15:36 -0300

    feat: add more tow errors test

--- src/domain/repositories/updateNotesRepository/updateNotesRepository.spec.ts
+++ src/domain/repositories/updateNotesRepository/updateNotesRepository.spec.ts
@@ -6,27 +6,50 @@ import { DatabaseSpy } from "../mocks/repository/DatabaseSpy";
 import { Database } from "../mocks/repository/interfaces/Database";
 
 interface IUpdateNotesRepository {
-  updateNote(noteId: string, newNote: NewNote): Promise<{updatedNote: Note | null, error: IError | null}>;
+  updateNote(
+    noteId: string,
+    newNote: NewNote
+  ): Promise<{ updatedNote: Note | null; error: IError | null }>;
 }
 
 class UpdateNotesRepository implements IUpdateNotesRepository {
   constructor(private databaseSpy: Database) {}
 
-  async updateNote(noteId: string, newNote: NewNote): Promise<{updatedNote: Note | null, error: IError | null}> {
+  async updateNote(
+    noteId: string,
+    newNote: NewNote
+  ): Promise<{ updatedNote: Note | null; error: IError | null }> {
     if (!newNote.author) {
-        return {
-            updatedNote: null,
-            error: {
-                message: new ProvidedParamsError('author'),
-                code: 500
-            }
-        }
+      return {
+        updatedNote: null,
+        error: {
+          message: new ProvidedParamsError("author"),
+          code: 500,
+        },
+      };
+    } else if (!newNote.content) {
+      return {
+        updatedNote: null,
+        error: {
+          message: new ProvidedParamsError("content"),
+          code: 500,
+        },
+      };
+    } else if (!newNote.title) {
+      return {
+        updatedNote: null,
+        error: {
+          message: new ProvidedParamsError("title"),
+          code: 500,
+        },
+      };
     }
+
     const updatedNote = await this.databaseSpy.update(noteId, newNote);
 
     return {
-        updatedNote,
-        error: null
+      updatedNote,
+      error: null,
     };
   }
 }
@@ -63,7 +86,7 @@ describe("Update Notes Repository", () => {
     };
 
     await databaseSpy.create(mockNewNote);
-    const {updatedNote} = await sut.updateNote(mockNoteId, mockUpdateNote);
+    const { updatedNote } = await sut.updateNote(mockNoteId, mockUpdateNote);
 
     expect(updatedNote).toEqual(mockUpdatedNote);
   });
@@ -85,6 +108,46 @@ describe("Update Notes Repository", () => {
     const { error } = await sut.updateNote(mockNoteId, mockUpdateNote);
 
     expect(error?.message.message).toBe("parameter: author, not provided");
+    expect(error?.code).toBe(500);
+  });
+  it("should return a 500 if content param is not provided correctly", async () => {
+    const { sut, databaseSpy } = makeSut();
+    const mockNoteId = "1";
+    const mockNewNote: NewNote = {
+      author: "any_author",
+      content: "any_content",
+      title: "any_title",
+    };
+    const mockUpdateNote: NewNote = {
+      author: "any_author",
+      content: "",
+      title: "other_title",
+    };
+
+    await databaseSpy.create(mockNewNote);
+    const { error } = await sut.updateNote(mockNoteId, mockUpdateNote);
+
+    expect(error?.message.message).toBe("parameter: content, not provided");
+    expect(error?.code).toBe(500);
+  });
+  it("should return a 500 if title param is not provided correctly", async () => {
+    const { sut, databaseSpy } = makeSut();
+    const mockNoteId = "1";
+    const mockNewNote: NewNote = {
+      author: "any_author",
+      content: "any_content",
+      title: "any_title",
+    };
+    const mockUpdateNote: NewNote = {
+      author: "any_author",
+      content: "other_author",
+      title: "",
+    };
+
+    await databaseSpy.create(mockNewNote);
+    const { error } = await sut.updateNote(mockNoteId, mockUpdateNote);
+
+    expect(error?.message.message).toBe("parameter: title, not provided");
     expect(error?.code).toBe(500);
   });
 });
