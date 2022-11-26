2133295e4c4bce26a0ac1f7e31beb33b75f9ead2
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-26 16:54:57 -0300

    feat: delete save-notes mocks

--- src/domain/usecases/saveNotesUseCase/mocks/repository/interfaces/SaveNotesRepository.ts
+++
@@ -1,8 +0,0 @@
-import { IError } from "../../../../../helpers/errors/saveNotesUseCaseError/interfaces/IError";
-import { Note } from "../../../../../models/Note";
-import { NewNote } from "../../../interfaces/iNewNote";
-
-export interface SaveNotesRepository {
-  save(newNote: NewNote): Promise<{note: Note | null, error: IError | null}>;
-  getNoteById(noteId: string): Note | null;
-}
--- src/domain/usecases/saveNotesUseCase/mocks/repository/interfaces/UpdateNotesRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Note } from "../../../../../models/Note";
-import { UpdateNote } from "../../../interfaces/IUpdateNote";
-
-export interface UpdateNotesRepository {
-  update(id: string, updatedNote: UpdateNote): Promise<Note | null>;
-}
