256002f8e70e2f4004b407fd2fa78c1103868bbd
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-26 20:12:01 -0300

    feat: add error test to trash notes controller

--- src/presentation/controllers/changeStatusNotesToTrashController/changeStatusNotesToTrashController.spec.ts
+++ src/presentation/controllers/changeStatusNotesToTrashController/changeStatusNotesToTrashController.spec.ts
@@ -12,12 +12,21 @@ import { SaveNotesController } from "../saveNotesController/SaveNotesController"
 import { SaveNotesUseCase } from "../../../domain/usecases/saveNotesUseCase/SaveNotesUseCase";
 import { SaveNotesRepository } from "../../../domain/repositories/saveNotesRepository/SaveNotesRepository";
 import { UpdateNotesRepository } from "../../../domain/repositories/updateNotesRepository/UpdateNotesRepository";
+import { ProvidedParamsError } from "../../../domain/helpers/errors/saveNotesUseCaseError/ProviedParamsError";
 
 class StatusNoteToTrashController implements IController {
     constructor (private statusNoteToTrashUseCase: IChangeStatusNotesToTrashUseCase) {}
 
   async route(request: IHttpRequest): Promise<IHttpResponse> {
     const {noteId} = request.params
+
+    if (!noteId) return {
+        error: {
+            message: new ProvidedParamsError('noteId')
+        },
+        code: 400,
+        response: null
+    }
 
     const {trashedNote} = await this.statusNoteToTrashUseCase.changeStatusNotesToTrashUseCase(noteId)
 
@@ -78,4 +87,19 @@ describe("Change Status Note to Trash Controller", () => {
     expect(response.response.trashedNote).toEqual(responseResult);
     expect(response.code).toBe(200)
   });
+  it('should return and error with code 400 if note id is note provided', async () => {
+    const {sut,saveNotesController} = makeSut()
+    const request: IHttpRequest = {
+      body: null,
+      params: {
+        noteId: "",
+      },
+    };
+
+    const response = await sut.route(request);
+
+    expect(response.error).toBeTruthy();
+    expect(response.error?.message.message).toBe("parameter: noteId, not provided");
+    expect(response.code).toBe(400)
+  })
 });
