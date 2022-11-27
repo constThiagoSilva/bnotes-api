31262679a13a466d45fff61f283ea4e48b9f810b
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-27 18:45:42 -0300

    feat: add instance helper to get-all-notes repo

---
+++ src/domain/repositories/getAllNotesRepository/helper/instanceGetAllNotesRepository.ts
@@ -0,0 +1,13 @@
+import { DatabaseSpy } from "../../mocks/repository/DatabaseSpy";
+import { databaseSpy } from "../../mocks/repository/helper/instanceDatabaseSpy";
+import { GetAllNotesRepository } from "../GetAllNotesRepository";
+
+export const instanceGetAllNotesRepository = () => {
+  const getAllNotesRepository = new GetAllNotesRepository(databaseSpy);
+
+  return getAllNotesRepository;
+};
+
+const getAllNotesRepository = instanceGetAllNotesRepository()
+
+export {getAllNotesRepository}
