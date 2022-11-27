1ddbc933d555d17ea95f29b523fdbdf9c0ccb02f
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-27 18:56:26 -0300

    feat: add instance helper to get-notes controller

--- src/presentation/controllers/getAllNotesController/helper/instanceGetAllNotesController.ts
+++ src/presentation/controllers/getAllNotesController/helper/instanceGetAllNotesController.ts
@@ -1,15 +1,12 @@
-import { GetAllNotesRepository } from "../../../../domain/repositories/getAllNotesRepository/GetAllNotesRepository";
-import { DatabaseSpy } from "../../../../domain/repositories/mocks/repository/DatabaseSpy";
-import { GetAllNotesUseCase } from "../../../../domain/usecases/getAllNotesUseCase/GetAllNotesUseCase";
+import { getAllNotesUseCase } from "../../../../domain/usecases/getAllNotesUseCase/helper/instanceGetAllNotesUseCase";
 import { GetAllNotesController } from "../GetAllNotesController";
 
-export const makeSut = () => {
-  const databaseSpy = new DatabaseSpy();
-  const getAllNotesUseCase = new GetAllNotesUseCase(
-    new GetAllNotesRepository(databaseSpy)
-  );
-
+export const instanceGetAllNotesController = () => {
   const getAllNotesController = new GetAllNotesController(getAllNotesUseCase);
 
   return getAllNotesController;
 };
+
+const getAllNotesController = instanceGetAllNotesController();
+
+export { getAllNotesController };
