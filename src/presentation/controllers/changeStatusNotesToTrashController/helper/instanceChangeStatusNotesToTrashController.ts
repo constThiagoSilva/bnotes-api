47fe2971dadafc3aa7ac13cbe8d05943bf01f360
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-27 18:55:41 -0300

    feat: add instance helper to trash-notes controller

--- src/presentation/controllers/changeStatusNotesToTrashController/helper/instanceChangeStatusNotesToTrashController.ts
+++ src/presentation/controllers/changeStatusNotesToTrashController/helper/instanceChangeStatusNotesToTrashController.ts
@@ -1,20 +1,14 @@
-import { ChangeStatusNotesToTrashRepository } from "../../../../domain/repositories/changeStatusNotesToTrashRepository/ChangeStatusNotesToTrashRepository";
-import { DatabaseSpy } from "../../../../domain/repositories/mocks/repository/DatabaseSpy";
-import { SaveNotesRepository } from "../../../../domain/repositories/saveNotesRepository/SaveNotesRepository";
-import { UpdateNotesRepository } from "../../../../domain/repositories/updateNotesRepository/UpdateNotesRepository";
-import { ChangeStatusNotesToTrashUseCase } from "../../../../domain/usecases/changeStatusNotesToTrashUseCase/ChangeStatusNotesToTrashUseCase";
-import { SaveNotesUseCase } from "../../../../domain/usecases/saveNotesUseCase/SaveNotesUseCase";
-import { SaveNotesController } from "../../saveNotesController/SaveNotesController";
+import { changeStatusNotesToTrashUseCase } from "../../../../domain/usecases/changeStatusNotesToTrashUseCase/helper/instanceChangeStatusNotesToTrashUseCase";
 import { StatusNoteToTrashController } from "../ChangeStatusNotesToTrash";
 
-export const makeSut = () => {
-  const databaseSpy = new DatabaseSpy();
-  const changeStatusNotesToTrashRepository =
-    new ChangeStatusNotesToTrashRepository(databaseSpy);
-  const changeStatusNotesToTrashUseCase = new ChangeStatusNotesToTrashUseCase(
-    changeStatusNotesToTrashRepository
+export const instanceStatusNoteToTrashController = () => {
+  const statusNoteToTrashControllert = new StatusNoteToTrashController(
+    changeStatusNotesToTrashUseCase
   );
-  const statusNoteToTrashControllert = new StatusNoteToTrashController(changeStatusNotesToTrashUseCase);
 
-  return statusNoteToTrashControllert ;
+  return statusNoteToTrashControllert;
 };
+
+const statusNoteToTrashControllert = instanceStatusNoteToTrashController();
+
+export { statusNoteToTrashControllert };
