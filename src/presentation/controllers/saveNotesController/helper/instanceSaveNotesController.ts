b97786a5845f812a08357b00508939553d0c281b
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-27 18:57:16 -0300

    feat: add instance helper to ave-notes controller

--- src/presentation/controllers/saveNotesController/helper/instanceSaveNotesController.ts
+++ src/presentation/controllers/saveNotesController/helper/instanceSaveNotesController.ts
@@ -1,16 +1,12 @@
-import { DatabaseSpy } from "../../../../domain/repositories/mocks/repository/DatabaseSpy";
-import { SaveNotesRepository } from "../../../../domain/repositories/saveNotesRepository/SaveNotesRepository";
-import { UpdateNotesRepository } from "../../../../domain/repositories/updateNotesRepository/UpdateNotesRepository";
-import { SaveNotesUseCase } from "../../../../domain/usecases/saveNotesUseCase/SaveNotesUseCase";
+import { saveNotesUseCase } from "../../../../domain/usecases/saveNotesUseCase/helper/instanceSaveNotesUseCase";
 import { SaveNotesController } from "../SaveNotesController";
 
 export const instanceSaveNotesController = () => {
-  const saveNotesController = new SaveNotesController(
-    new SaveNotesUseCase(
-      new SaveNotesRepository(new DatabaseSpy()),
-      new UpdateNotesRepository(new DatabaseSpy())
-    )
-  );
+  const saveNotesController = new SaveNotesController(saveNotesUseCase);
 
   return saveNotesController;
 };
+
+const saveNotesController = instanceSaveNotesController();
+
+export { saveNotesController };
