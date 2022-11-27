77531a147729f8642a21fe80e14e13d5311ba502
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-27 18:58:18 -0300

    fix: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,15 @@
 import express, { Router } from "express";
 import { DatabaseSpy } from "./domain/repositories/mocks/repository/DatabaseSpy";
-import { SaveNotesRepository } from "./domain/repositories/saveNotesRepository/SaveNotesRepository";
-import { UpdateNotesRepository } from "./domain/repositories/updateNotesRepository/UpdateNotesRepository";
-import { SaveNotesUseCase } from "./domain/usecases/saveNotesUseCase/SaveNotesUseCase";
-import { instanceSaveNotesController } from "./presentation/controllers/saveNotesController/helper/instanceSaveNotesController";
-import { SaveNotesController } from "./presentation/controllers/saveNotesController/SaveNotesController";
+import { saveNotesController } from "./presentation/controllers/saveNotesController/helper/instanceSaveNotesController";
 
 const app = express();
 const router = Router();
-const databaseSpy = new DatabaseSpy();
 
 app.use(express.json());
 
 router.get("/", (request, response) => response.send("ola"));
 router.post("/notes/save", async (request, response) => {
-  const result = await instanceSaveNotesController().route(request)
+  const result = await saveNotesController.route(request)
 
   return response.status(result.code).json(result.response)
 });
